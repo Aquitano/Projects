74e94e7fe76bdaf51a831b6e3b600ade86cc1cb8
Aquitano <aquitano@protonmail.com>
2021-12-10 14:35:47 +0100

    Added basic structure - added matrix & vector - basic structure for class

---
+++ FlowerClassification/main.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+a = 2 # sepal_length
+b = 2 # sepal_width
+c = 2 # petal_length
+d = 2 # petal_width 
+
+input = np.array([a, b, c, d])
+
+matrix = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+
+print(np.dot(matrix, input))
+
+# neural network class definition
+class neuralNetwork:
+
+    # initialize the neural network
+    def __init__():
+        pass
+
+    # train the neural network
+    def train():
+        pass
+
+    # query the neural network
+    def query():
+        pass
